35746022fcd9b9ef68fde2384d3b54f99343114b
Emi Yoshihara <glorypulse@gmail.com>
2016-07-25 23:19:43 +0900

    Studied PCC Book until p57

---
+++ study/LCS.java
@@ -0,0 +1,53 @@
+package study;
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+/** Programming Contest Challenge Book p56
+    LCS Problem
+ */
+public class LCSProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        String s = sc.next();
+        String t = sc.next();
+
+        LCSProblem lcsp = new LCSProblem();
+        int ans = lcsp.solve(s, t);
+
+        pw.println(String.valueOf(ans));
+        pw.flush();
+        pw.close();
+            
+    }
+
+    int solve(String s, String t) {
+        int[][] lcs = new int[s.length()][t.length()];
+        // initialize
+        for (int i = 0; i < s.length(); i++) {
+            lcs[i][0] = 0;
+        }
+        for (int j = 1; j < t.length(); j++) {
+            lcs[0][j] = 0;
+        }
+
+        // dynamic programming
+        for (int i = 1; i < s.length(); i++) {
+            for (int j=1; j < t.length(); j++) {
+                if (s[i] == t[i]) {
+                    lcs[i][j] = Math.max(lcs[i-1][j-1]+1, lcs[i-1][j], lcs[i][j-1]);
+                } else {
+                    lcs[i][j] = Math.max(lcs[i-1][j], lcs[i][j-1]);
+                }
+            }
+        }
+
+        return lcs[s.length()-1][t.length()-1];
+    }
+    
+}
+    
