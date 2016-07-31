fde3c6b09183c98700af588f33695a4c71a9e2f9
Emi Yoshihara <glorypulse@gmail.com>
2016-07-31 16:44:29 +0900

    add scala file

---
+++ study/POJ3255.scala
@@ -0,0 +1,13 @@
+package study
+
+/** Programming Contest Challenge Book p102
+  POJ No.3255 Roadblocks
+  http://poj.org/problem?id=3255
+  */
+
+object POJ3255 {
+  def main(args : Array[String]) {
+
+
+
+}
