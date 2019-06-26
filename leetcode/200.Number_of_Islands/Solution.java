5230a91361cc7772ac21f5823ebf5148e658cec9
Emi Yoshihara <glorypulse@gmail.com>
2019-06-26 16:32:41 +0900

    optimize 200. Numbers of islands

--- leetcode/200.Number_of_Islands/Solution.java
+++ leetcode/200.Number_of_Islands/Solution.java
@@ -1,27 +1,26 @@
-import java.util.HashSet;
-
 class Solution {
     public int numIslands(char[][] grid) {
-        HashSet<String> touched = new HashSet<String>(); // booleanの配列で代替できる
+        if (grid.length == 0) return 0;
+        boolean[][] touched = new boolean[grid.length][grid[0].length];
         int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (touched.contains(key(i, j))) continue;
+                if (touched[i][j]) continue;
                 if (grid[i][j] == '1') {
                     // found an island!
                     count++;
                     foundIsland(grid, touched, i, j);
                 } else {
-                    touched.add(key(i, j));
+                    touched[i][j] = true;
                 }
             }
         }
         return count;
     }
 
-    private void foundIsland(char[][] grid, HashSet<String> touched, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || touched.contains(key(i, j))) return;
-        touched.add(key(i, j));
+    private void foundIsland(char[][] grid, boolean[][] touched, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || touched[i][j]) return;
+        touched[i][j] = true;
         if (grid[i][j] == '0') return;
         if (grid[i][j] == '1') {
             // right
@@ -34,9 +33,5 @@ class Solution {
             foundIsland(grid, touched, i - 1, j);
         }
         return;
-    }
-
-    private String key(int i, int j) {
-        return i + "," + j;
     }
 }
