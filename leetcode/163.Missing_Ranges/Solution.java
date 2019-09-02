f42bad44df868cfbfb07ba1d9ae5b298fa64d28a
Emi Yoshihara <glorypulse@gmail.com>
2019-09-02 22:37:27 +0900

    solve 163. Missing Ranges

--- leetcode/163.Missing_Ranges/Solution.java
+++ leetcode/163.Missing_Ranges/Solution.java
@@ -1,5 +1,26 @@
-class Solution {
-    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+import java.util.ArrayList;
 
+class Solution {
+    String range(int start, int end) {
+        if (start == end) {
+            return String.valueOf(start);
+        }
+        return start + "->" + end;
+    }
+
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        ArrayList<String> ret = new ArrayList<String>();
+        int start = lower;
+        for (int num: nums) {
+            if (num > start) {
+                ret.add(range(start, num - 1));
+            }
+            if (num == Integer.MAX_VALUE) { return ret; }
+            start = num + 1;
+        }
+        if (upper >= start) {
+            ret.add(range(start, upper));
+        }
+        return ret;
     }
 }
