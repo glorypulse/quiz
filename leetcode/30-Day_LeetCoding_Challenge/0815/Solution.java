55e5feceed4a292b36edc99849a77c90c6eb82f3
Emi Yoshihara <glorypulse@gmail.com>
2020-08-16 19:33:01 +0900

    30 days challenge August 15th, in LeetCode

---
+++ leetcode/30-Day_LeetCoding_Challenge/0815/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) return 0;
+
+        Arrays.sort(intervals, (a, b) -> a[1] == b[1] ? b[0] - a[0] : a[1] - b[1]);
+
+        int removalCount = 0;
+        int prevEnd = Integer.MIN_VALUE;
+        for (int[] interval : intervals) {
+            if (prevEnd > interval[0]) {
+                removalCount ++;
+            } else {
+                prevEnd = interval[1];
+            }
+        }
+
+        return removalCount;
+    }
+}
