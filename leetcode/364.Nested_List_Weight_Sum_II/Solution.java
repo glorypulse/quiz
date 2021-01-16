ccac0b5c9d48401703a3f16f801970b4f342aecf
Emi Yoshihara <glorypulse@gmail.com>
2021-01-16 22:29:16 +0900

    solve 364. Nested List Weight Sum II in LeetCode

---
+++ leetcode/364.Nested_List_Weight_Sum_II/Solution.java
@@ -0,0 +1,54 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        Queue<List<NestedInteger>> queue = new LinkedList<>();
+        queue.offer(nestedList);
+
+        int total = 0;
+        int diff = 0;
+        while (!queue.isEmpty()) {
+            int sum = 0;
+            int size = queue.size();
+            for (int i = 0; i < size; i ++) {
+                List<NestedInteger> list = queue.poll();
+                for (NestedInteger cur: list) {
+                    if (cur.isInteger()) {
+                        sum += cur.getInteger();
+                    } else {
+                        queue.offer(cur.getList());
+                    }
+                }
+            }
+            total += diff + sum;
+            diff += sum;
+        }
+        return total;
+    }
+}
