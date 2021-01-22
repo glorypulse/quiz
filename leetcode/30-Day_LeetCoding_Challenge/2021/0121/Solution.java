3f0a0a0af8af3225375b76391a875a64a3a1515f
Emi Yoshihara <glorypulse@gmail.com>
2021-01-22 23:11:24 +0900

    improve code in LeetCode

--- leetcode/30-Day_LeetCoding_Challenge/2021/0121/Solution.java
+++ leetcode/30-Day_LeetCoding_Challenge/2021/0121/Solution.java
@@ -1,44 +1,63 @@
 class Solution {
     public int[] mostCompetitive(int[] nums, int k) {
         int length = nums.length;
-        if (length == 0) return nums;
-        int[][] numsWithIndex = new int[length][];
+        if (length == 0) return new int[0];
+
+        int[] subsequence = new int[k];
+        int index = 0;
         for (int i = 0; i < length; i ++) {
-            numsWithIndex[i] = new int[] { nums[i], i };
-        }
-        Arrays.sort(numsWithIndex, (a, b) -> {
-                if (a[0] != b[0]) {
-                    return a[0] - b[0];
-                }
-                return a[1] - b[1];
+            int num = nums[i];
+            while (index > Math.max(0, k - length + i) && subsequence[index - 1] > num) {
+                index --;
             }
-            );
-        int[][] subsequenceWithIndex = new int[k][];
-        int index = -1;
-        int j = 0;
-        for (int i = 0; i < k; i ++) {
-            if (subsequenceWithIndex[i] != null) {
-                index = subsequenceWithIndex[i][1];
-                continue;
-            }
-            while (j < length) {
-                int nextIndex = numsWithIndex[j][1];
-                if (index < nextIndex) {
-                    if (nextIndex <= length - k + i) {
-                        subsequenceWithIndex[i] = numsWithIndex[j];
-                        index = nextIndex;
-                        break;
-                    } else {
-                        subsequenceWithIndex[nextIndex - length + k] = numsWithIndex[j];
-                    }
-                }
-                j ++;
+            if (index < k) {
+                subsequence[index] = num;
+                index ++;
             }
         }
-        int[] ans = new int[k];
-        for (int i = 0; i < k; i ++) {
-            ans[i] = subsequenceWithIndex[i][0];
-        }
-        return ans;
+        return subsequence;
     }
+
+    // public int[] mostCompetitive(int[] nums, int k) {
+    //     int length = nums.length;
+    //     if (length == 0) return nums;
+    //     int[][] numsWithIndex = new int[length][];
+    //     for (int i = 0; i < length; i ++) {
+    //         numsWithIndex[i] = new int[] { nums[i], i };
+    //     }
+    //     Arrays.sort(numsWithIndex, (a, b) -> {
+    //             if (a[0] != b[0]) {
+    //                 return a[0] - b[0];
+    //             }
+    //             return a[1] - b[1];
+    //         }
+    //         );
+    //     int[][] subsequenceWithIndex = new int[k][];
+    //     int index = -1;
+    //     int j = 0;
+    //     for (int i = 0; i < k; i ++) {
+    //         if (subsequenceWithIndex[i] != null) {
+    //             index = subsequenceWithIndex[i][1];
+    //             continue;
+    //         }
+    //         while (j < length) {
+    //             int nextIndex = numsWithIndex[j][1];
+    //             if (index < nextIndex) {
+    //                 if (nextIndex <= length - k + i) {
+    //                     subsequenceWithIndex[i] = numsWithIndex[j];
+    //                     index = nextIndex;
+    //                     break;
+    //                 } else {
+    //                     subsequenceWithIndex[nextIndex - length + k] = numsWithIndex[j];
+    //                 }
+    //             }
+    //             j ++;
+    //         }
+    //     }
+    //     int[] ans = new int[k];
+    //     for (int i = 0; i < k; i ++) {
+    //         ans[i] = subsequenceWithIndex[i][0];
+    //     }
+    //     return ans;
+    // }
 }
