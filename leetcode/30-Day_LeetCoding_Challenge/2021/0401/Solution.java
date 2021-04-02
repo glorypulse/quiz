f99b3a17f7d8726fbb8ea375ca1fd941fe6906ff
Emi Yoshihara <glorypulse@gmail.com>
2021-04-02 15:09:33 +0900

    30 days challenge April 1st, in LeetCode

---
+++ leetcode/30-Day_LeetCoding_Challenge/2021/0401/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while (fast != null) {
+            if (fast.next == null) {
+                slow = slow.next;
+                break;
+            }
+            fast = fast.next.next;
+            ListNode tmp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = tmp;
+        }
+
+        while (slow != null) {
+            if (slow.val != prev.val) {
+                return false;
+            }
+            slow = slow.next;
+            prev = prev.next;
+        }
+        return true;
+    }
+}
