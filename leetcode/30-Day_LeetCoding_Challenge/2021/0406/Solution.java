60b1f6edeecd7a8d9328dc3c95c84bd465b747e3
Emi Yoshihara <glorypulse@gmail.com>
2021-04-08 15:06:02 +0900

    30 days challenge April 6th, in LeetCode

---
+++ leetcode/30-Day_LeetCoding_Challenge/2021/0406/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minOperations(int n) {
+        if (n % 2 == 0) {
+            return (n / 2) * (n / 2);
+        } else {
+            return (n / 2) * (n / 2 + 1);
+        }
+    }
+}
